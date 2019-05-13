6b2385d483ab2f376d4dc212132ae7878077c5a4
Oziel <ozielcarvalho@live.com>
2019-05-13 17:03:03 -0300

    Permissões acrescentadas.

--- app/src/main/java/com/example/tcc/MainActivity.java
+++ app/src/main/java/com/example/tcc/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.tcc;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,15 +8,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
+
 import com.priyankvasa.android.cameraviewex.CameraView;
-import com.priyankvasa.android.cameraviewex.Image;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraView mCameraView;
+    private String[] permissions = {Manifest.permission.CAMERA};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,51 +25,58 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         mCameraView = findViewById(R.id.camera);
 
+//        mCameraView.setOnClickListener(new View.OnClickListener() {
+//            @SuppressLint("MissingPermission")
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Tocou", Toast.LENGTH_SHORT).show();
+//                mCameraView.capture();
+//            }
+//        });
 
-        mCameraView.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Tocou", Toast.LENGTH_SHORT).show();
-                mCameraView.capture();
-            }
-        });
-
-        mCameraView.addPictureTakenListener(new Function1<Image, Unit>() {
-            @Override
-            public Unit invoke(Image image) {
-                Toast.makeText(MainActivity.this, "Bateu a foto", Toast.LENGTH_SHORT).show();
-                return null;
-            }
-        });
+//        mCameraView.addPictureTakenListener(new Function1<Image, Unit>() {
+//            @Override
+//            public Unit invoke(Image image) {
+//                Toast.makeText(MainActivity.this, "Bateu a foto", Toast.LENGTH_SHORT).show();
+//                return null;
+//            }
+//        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
+
             @Override
             public void onClick(View view) {
-                mCameraView.capture();
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+                }else{
+                    mCameraView.capture();
+                }
             }
         });
 
     }
 
-
     @Override
     public void onResume() {
         super.onResume();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        }else{
+            mCameraView.start();
         }
-        mCameraView.start();
+    }
 
+    @Override
+    public void onPause(){
+        mCameraView.stop();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy(){
+        mCameraView.destroy();
+        super.onDestroy();
     }
 
 }
